ed3cfafda5327f640da5ec21d5e25c3f3434802a
cousepig <cousepig@gmail.com>
2024-03-19 21:26:59 +0800

    1

--- server/api/sponsors.json.get.ts
+++ server/api/sponsors.json.get.ts
@@ -3,49 +3,49 @@ export default eventHandler(async (event) => {
 
   return [
     {
-      "platinum": [
+      'platinum': [
         {
-          "sponsorId": "netlify-bot",
-          "sponsorName": "netlibot",
-          "sponsorLogo": "https://avatars.githubusercontent.com/u/33071329?u=7981813b9db8b6de244a9f4dd70a82aad0566bf6&v=4",
-          "sponsorUrl": "https://www.netlify.com",
-          "monthlyPriceInDollars": 2500
+          'sponsorId': 'netlify-bot',
+          'sponsorName': 'netlibot',
+          'sponsorLogo': 'https://avatars.githubusercontent.com/u/33071329?u=7981813b9db8b6de244a9f4dd70a82aad0566bf6&v=4',
+          'sponsorUrl': 'https://www.netlify.com',
+          'monthlyPriceInDollars': 2500
         }
       ],
-      "gold": [
+      'gold': [
         {
-          "sponsorId": "curotec",
-          "sponsorName": "Curotec",
-          "sponsorLogo": "https://avatars.githubusercontent.com/u/34548260?v=4",
-          "sponsorUrl": "https://www.curotec.com",
-          "monthlyPriceInDollars": 500
+          'sponsorId': 'curotec',
+          'sponsorName': 'Curotec',
+          'sponsorLogo': 'https://avatars.githubusercontent.com/u/34548260?v=4',
+          'sponsorUrl': 'https://www.curotec.com',
+          'monthlyPriceInDollars': 500
         }
       ],
-      "silver": [
+      'silver': [
         {
-          "sponsorId": "reanmo",
-          "sponsorName": "reanmo",
-          "sponsorLogo": "https://avatars.githubusercontent.com/u/47215958?v=4",
-          "sponsorUrl": "https://reanmo.de",
-          "monthlyPriceInDollars": 250
+          'sponsorId': 'reanmo',
+          'sponsorName': 'reanmo',
+          'sponsorLogo': 'https://avatars.githubusercontent.com/u/47215958?v=4',
+          'sponsorUrl': 'https://reanmo.de',
+          'monthlyPriceInDollars': 250
         }
       ],
-      "bronze": [
+      'bronze': [
         {
-          "sponsorId": "sistersites",
-          "sponsorName": "Sister Sites",
-          "sponsorLogo": "https://avatars.githubusercontent.com/u/145147130?u=f41c86cc03389e46ca631406c11aeda5615c176f&v=4",
-          "sponsorUrl": "https://www.sister-sites.co.uk/",
-          "monthlyPriceInDollars": 100
+          'sponsorId': 'sistersites',
+          'sponsorName': 'Sister Sites',
+          'sponsorLogo': 'https://avatars.githubusercontent.com/u/145147130?u=f41c86cc03389e46ca631406c11aeda5615c176f&v=4',
+          'sponsorUrl': 'https://www.sister-sites.co.uk/',
+          'monthlyPriceInDollars': 100
         }
       ],
-      "backer": [
+      'backer': [
         {
-          "sponsorId": "justin-schroeder",
-          "sponsorName": "Justin Schroeder",
-          "sponsorLogo": "https://avatars.githubusercontent.com/u/1950463?v=4",
-          "sponsorUrl": "https://www.formkit.com",
-          "monthlyPriceInDollars": 10
+          'sponsorId': 'justin-schroeder',
+          'sponsorName': 'Justin Schroeder',
+          'sponsorLogo': 'https://avatars.githubusercontent.com/u/1950463?v=4',
+          'sponsorUrl': 'https://www.formkit.com',
+          'monthlyPriceInDollars': 10
         }
       ]
     }
