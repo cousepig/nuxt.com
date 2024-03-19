afc2cd024aeb1e9034bc3e672d6ef75864c80ff4
cousepig <cousepig@gmail.com>
2024-03-19 21:19:46 +0800

    1

--- server/api/jobs.json.get.ts
+++ server/api/jobs.json.get.ts
@@ -2,56 +2,56 @@ export default eventHandler(async (event) => {
   return [
     [
       {
-           "title": "中级前端开发人员👽​​",
-           "link": "https://vuejobs.com/jobs/vue-school-mid-level-frontend-developer?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list ",
-           "description":
-             "关于我们您好！我们是 BitterBrains，是热门学习平台 Vue School 和 Mastering Nuxt 背后的公司。我们也是 Vue.js 活动的组织者，包括 Nuxt N...",
-           "locations": [],
-           "remote": "ONLY",
-           "organization": {
-             "name": "Vue School",
-             "avatar":
-               "https://app.vuejobs.com/storage/294/d506e811-956a-43d3- a155-ea10fb24ccef.png"
+           'title': '中级前端开发人员👽​​',
+           'link': 'https://vuejobs.com/jobs/vue-school-mid-level-frontend-developer?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list ',
+           'description':
+             '关于我们您好！我们是 BitterBrains，是热门学习平台 Vue School 和 Mastering Nuxt 背后的公司。我们也是 Vue.js 活动的组织者，包括 Nuxt N...',
+           'locations': [],
+           'remote': 'ONLY',
+           'organization': {
+             'name': 'Vue School',
+             'avatar':
+               'https://app.vuejobs.com/storage/294/d506e811-956a-43d3- a155-ea10fb24ccef.png'
            },
-           "published_at": "2024-03-15T16:39:02+01:00"
+           'published_at': '2024-03-15T16:39:02+01:00'
          },
          {
-           "title": "前端开发人员（Vue.js 和 Nuxt.js）",
-           "link ":
-             "https://vuejobs.com/jobs/zapfloor-frontend-developer-vue-js-nuxt-js-2?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list",
-           "description":
-             "您将成为工程团队是一支快速发展且技术精湛的团队，正在构建出色的道具技术产品。您将与团队一起定义整体前端-...",
-           "locations": ["比利时安特卫普"],
-           "远程": "混合",
-           "组织": {
-             "名称": "zapfloor",
-             "头像": "https://app.vuejobs.com/storage/2245/abe6c294-b1c0-4763-86e0-a38542ed24c4.com"
+           'title': '前端开发人员（Vue.js 和 Nuxt.js）',
+           'link ':
+             'https://vuejobs.com/jobs/zapfloor-frontend-developer-vue-js-nuxt-js-2?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list',
+           'description':
+             '您将成为工程团队是一支快速发展且技术精湛的团队，正在构建出色的道具技术产品。您将与团队一起定义整体前端-...',
+           'locations': ['比利时安特卫普'],
+           '远程': '混合',
+           '组织': {
+             '名称': 'zapfloor',
+             '头像': 'https://app.vuejobs.com/storage/2245/abe6c294-b1c0-4763-86e0-a38542ed24c4.com'
            },
-           "published_at": "2024-03-13T12:41:00+01:00"
+           'published_at': '2024-03-13T12:41:00+01:00'
          },
          {
-           "title": "高级前端开发人员🚀",
-           "link": "https://vuejobs.com/jobs/ vue-school-senior-frontend-developer?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list",
-           "description":
-             "关于我们您好！我们是 BitterBrains，是流行学习平台 Vue School 和 Mastering Nuxt 背后的公司。我们也是 Vue.js 活动的组织者，包括 Nuxt N...",
-           "locations": [],
-           "remote": "ONLY",
-           "organization": {
-             "name": "Vue School",
-             "avatar ":
-               "https://app.vuejobs.com/storage/294/d506e811-956a-43d3-a155-ea10fb24ccef.png"
+           'title': '高级前端开发人员🚀',
+           'link': 'https://vuejobs.com/jobs/ vue-school-senior-frontend-developer?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list',
+           'description':
+             '关于我们您好！我们是 BitterBrains，是流行学习平台 Vue School 和 Mastering Nuxt 背后的公司。我们也是 Vue.js 活动的组织者，包括 Nuxt N...',
+           'locations': [],
+           'remote': 'ONLY',
+           'organization': {
+             'name': 'Vue School',
+             'avatar ':
+               'https://app.vuejobs.com/storage/294/d506e811-956a-43d3-a155-ea10fb24ccef.png'
            },
-           "published_at": "2024-03-12T10:20:28+01:00"
+           'published_at': '2024-03-12T10:20:28+01:00'
          },
          {
-           "title": "高级全栈工程师 - Vue.js/Nuxt",
-           "link": "https://vuejobs.com/jobs/slides-with-friends-senior-full-stack-engineer- vue-js-nuxt?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list",
-           "description":
-             "Slides With Friends 是一个用于互动演示、小组教育、现场投票/测验和团队建设的平台 - 我们让小组体验更具吸引力、教育和...",
-           "locations": [],
-           "remote": "ONLY",
-           "organization": {
-             "name": "与朋友一起幻灯片"
+           'title': '高级全栈工程师 - Vue.js/Nuxt',
+           'link': 'https://vuejobs.com/jobs/slides-with-friends-senior-full-stack-engineer- vue-js-nuxt?utm_medium=referral&utm_source=nuxtjs&utm_content=job_link&utm_campaign=job_list',
+           'description':
+             'Slides With Friends 是一个用于互动演示、小组教育、现场投票/测验和团队建设的平台 - 我们让小组体验更具吸引力、教育和...',
+           'locations': [],
+           'remote': 'ONLY',
+           'organization': {
+             'name': '与朋友一起幻灯片'
            }
          }
      ]
